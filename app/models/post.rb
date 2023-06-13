798eade47dc0d69c9dc52b9a342c3286ffae87ab
Ezra <ezraermy@gmail.com>
2023-06-14 00:53:50 +0300

    Use medels to insert data and create custom methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def update_user_post_counter
+    author.update(postCounter: author.posts.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
